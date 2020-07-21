f41ef0518923324e9f4018fb297a05d38b47df0a
Shoaib Sharif <sharifshoaib@yahoo.com>
2020-07-21 14:09:14 -0700

    fixed .env (and it works on my machine)

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -2,11 +2,9 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri:
-        process.env.NODE_ENV === 'production'
-            ? process.env.REACT_APP_API_URL
-            : 'http://localhost:4545/',
-    credentials: 'same-origin'
+    uri: process.env.REACT_APP_API_URL,
+
+    credentials: 'include'
 });
 
 const authLink = setContext((_, { headers }) => {
