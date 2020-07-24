0787cfa84eeaebcbe6ef69e1635352e0402c2aa4
bxzahid <bxzahid@gmail.com>
2020-07-24 15:54:14 +0600

    add history, react-perfect-scrollbar package

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Router } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './ApolloClient';
 import { createBrowserHistory } from 'history';
-import { Chart } from 'react-chartjs-2';
+// import { Chart } from 'react-chartjs-2';
 import { ThemeProvider } from '@material-ui/styles';
 import validate from 'validate.js';
 
-import { chartjs } from './helpers';
+// import { chartjs } from './helpers';
 import theme from './theme';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import './assets/scss/index.scss';
@@ -16,9 +16,9 @@ import Routes from './router/Routes';
 
 const browserHistory = createBrowserHistory();
 
-Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
-    draw: chartjs.draw
-});
+// Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
+//     draw: chartjs.draw
+// });
 
 validate.validators = {
     ...validate.validators,
