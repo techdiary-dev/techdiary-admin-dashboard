508c46e33ae56629f8e50471c0f5e2f2d7293b48
bxzahid <bxzahid@gmail.com>
2020-08-03 05:56:45 +0600

    feat(TotalUser): add a circular progress

--- src/components/Dashboard/TotalUser.js
+++ src/components/Dashboard/TotalUser.js
@@ -5,6 +5,7 @@ import { useQuery } from '@apollo/client';
 import { makeStyles } from '@material-ui/styles';
 import { Card, CardContent, Grid, Typography, Avatar } from '@material-ui/core';
 import PeopleIcon from '@material-ui/icons/People';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { TOTAL_USER } from '../../quries/USER';
 
@@ -50,7 +51,11 @@ const TotalUser = (props) => {
                             TOTAL USER
                         </Typography>
                         <Typography variant="h3">
-                            {data?.getAllUsers.resourceCount}
+                            {data ? (
+                                data.getAllUsers.resourceCount
+                            ) : (
+                                <CircularProgress color="secondary" size={17} />
+                            )}
                         </Typography>
                     </Grid>
                     <Grid item>
