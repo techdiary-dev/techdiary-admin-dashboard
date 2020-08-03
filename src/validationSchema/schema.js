76147a8bafa57e7c38714bef86514bbcdbaf1d65
bxzahid <bxzahid@gmail.com>
2020-08-03 09:51:32 +0600

    fix(schema): add trim()

--- src/validationSchema/schema.js
+++ src/validationSchema/schema.js
@@ -1,9 +1,9 @@
 import * as yup from 'yup';
 
 export const profileSchema = yup.object().shape({
-    name: yup.string().required('Name is required'),
-    username: yup.string().required('Username is required'),
-    email: yup.string().required().email('Email must be a valid email')
+    name: yup.string().trim().required('Name is required'),
+    username: yup.string().trim().required('Username is required'),
+    email: yup.string().trim().required().email('Email must be a valid email')
 });
 
 export const passwordSchema = yup.object().shape({
@@ -13,26 +13,28 @@ export const passwordSchema = yup.object().shape({
 });
 
 export const signInSchema = yup.object().shape({
-    identifier: yup.string().required('Identifier is required'),
+    identifier: yup.string().trim().required('Identifier is required'),
     password: yup.string().required('Password is required')
 });
 
 export const signUpSchema = yup.object().shape({
-    name: yup.string().required('Name is required'),
-    username: yup.string().required('Username is required'),
+    name: yup.string().trim().required('Name is required'),
+    username: yup.string().trim().required('Username is required'),
     email: yup
         .string()
         .email('Email must be a valid email')
+        .trim()
         .required('Email is required'),
     password: yup.string().required('Password is required')
 });
 
 export const createAdminSchema = yup.object().shape({
-    name: yup.string().required('Name is required'),
-    username: yup.string().required('Username is required'),
+    name: yup.string().trim().required('Name is required'),
+    username: yup.string().trim().required('Username is required'),
     email: yup
         .string()
         .email('Email must be a valid email')
+        .trim()
         .required('Email is required'),
     password: yup.string().required('Password is required')
 });
